2a1efa3137d35f7d16df4fe239df8fa8f901340d
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-04-24 16:03:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,14 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h"
+
 std::string infx2pstfx(std::string inf) {
-  int PrOp(char z){
-    if((z == '/')||(z == '*')){return 3;}
-    else if((z == '+')||(z == '-')){return 2;}
-    else if(z == ')'){return 1;}
-    else if(z == '(') {return 0;}
-    else return -1;
+int PrOp(char z){
+if((z == '/')||(z == '*')){return 3;}
+else if((z == '+')||(z == '-')){return 2;}
+else if(z == ')'){return 1;}
+else if(z == '('){return 0;}
+else return -1;
 }
 
 std::string infx2pstfx(std::string inf) {
@@ -20,77 +21,67 @@ char temp = 0;
 char temp2 = 0;
 char temp3 = 0;
 
-  while(i < inf.size()-1){
-      
-       if(PrOp(inf[i]) == -1){
-           inf1.push(inf[i]);
-           i++;
-       } else if((PrOp(inf[i]) == 3)||(PrOp(inf[i]) == 2)){
-           if(PrOp(inf[i+1]) != 0){
-               temp = inf[i];
-               i++;
-           } else {//may be error
-               if((PrOp(inf[i-3]) == 1)&&(PrOp(inf[i-2]) == 3)){
-                   temp3 = inf[i];
-                   i++;
-               } else {
-               temp2 = inf[i];
-               i++;
-               }
-           }
-       } else if(PrOp(inf[i]) == 0){
-           i++;
-           while(PrOp(inf[i]) != 1){
-               if(PrOp(inf[i]) == -1){
-                    inf1.push(inf[i]);
-                    i++;
-                } else if((PrOp(inf[i]) == 3)||(PrOp(inf[i]) == 2)){
-                    temp = inf[i];
-                    i++;
-                }
-           }
-           if(PrOp(inf[i]) == 1){
-                    inf1.push(temp);
-                    temp = 0;
-                    i++;
-            }
-           
-           if(temp2 != 0){
-                
-                 inf1.push(temp2);
-                 temp2 =0;
-                
-           }
-       }
-       if((temp!= 0)&&(PrOp(inf[i]) == -1)){
-               inf1.push(inf[i]);
-                inf1.push(temp);
-                temp = 0;
-                i++;
-       }
-      
-  }
-   if(temp3 != 0){
-       inf1.push(temp3);
-       }
-       
-
-  while(inf1.isEmpty() == false){
-      inf2 += inf1.get();
-      inf1.pop();
-  }
-  
-  i1 = inf2.size()-1;
-  while(i1 > -1){
-     inf3 += inf2[i1];
-     if(i1 > 0){
-     inf3 += ' ';
-     }
-     i1--;
- }
-  
- 
-  return inf3;
+while(i < inf.size()-1){      
+if(PrOp(inf[i]) == -1){
+inf1.push(inf[i]);
+i++;
+} else if((PrOp(inf[i]) == 3)||(PrOp(inf[i]) == 2)){
+if(PrOp(inf[i+1]) != 0){
+temp = inf[i];
+i++;
+} else {
+if((PrOp(inf[i-3]) == 1)&&(PrOp(inf[i-2]) == 3)){
+temp3 = inf[i];
+i++;
+} else {
+temp2 = inf[i];
+i++;
+}
+}
+} else if(PrOp(inf[i]) == 0){
+i++;
+while(PrOp(inf[i]) != 1){
+if(PrOp(inf[i]) == -1){
+inf1.push(inf[i]);
+i++;
+} else if((PrOp(inf[i]) == 3)||(PrOp(inf[i]) == 2)){
+temp = inf[i];
+i++;
+}
+}
+if(PrOp(inf[i]) == 1){
+inf1.push(temp);
+temp = 0;
+i++;
+}
+if(temp2 != 0){
+inf1.push(temp2);
+temp2 =0;
+}
+}
+if((temp!= 0)&&(PrOp(inf[i]) == -1)){
+inf1.push(inf[i]);
+inf1.push(temp);
+temp = 0;
+i++;
+}
+}
+if(temp3 != 0){
+inf1.push(temp3);
+}
+while(inf1.isEmpty() == false){
+inf2 += inf1.get();
+inf1.pop();
+} 
+i1 = inf2.size()-1;
+while(i1 > -1){
+inf3 += inf2[i1];
+if(i1 > 0){
+inf3 += ' ';
+}
+i1--;
+} 
+return inf3;
 }
 
 
