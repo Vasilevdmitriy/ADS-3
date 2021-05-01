89137b10d8b702df20b29f9d403351fcff90d022
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-05-01 11:51:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 #include "tstack.h"
 
 int PrOp(char z){
-if((z == '/')||(z == '*')){return 3;}
-else if((z == '+')||(z == '-')){return 2;}
+if((z == '/') || (z == '*')) {return 3;}
+else if((z == '+') || (z == '-')){return 2;}
 else if(z == ')'){return 1;}
 else if(z == '('){return 0;}
 else return -1;
